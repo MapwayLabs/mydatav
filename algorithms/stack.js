ac4e6de83d852239d8c55b5d3e7059a4cb5d2046
linghuam <1551827898@qq.com>
2019-03-15 20:13:08 +0800

    hashTable

--- algorithms/stack.js
+++ algorithms/stack.js
@@ -13,6 +13,13 @@ Stack.prototype = {
     pop: function() {
         return this._stack.pop();
     },
+    
+    // 取栈顶
+    peek: function() {
+        if (this._stack.length) {
+            return this._stack[this._stack.length - 1];
+        }
+    },
 
     print: function() {
         return this._stack.toString();
