a00adb12da78940c88393632b8ad6027c844861b
linghuam <1551827898@qq.com>
2019-03-25 19:42:50 +0800

    hashTable

--- algorithms/hashTable.js
+++ algorithms/hashTable.js
@@ -1,6 +1,6 @@
 const defaultHashTableSize = 32;
 function HashTable(hashTableSize = defaultHashTableSize) {
-    this.buckets = new Array(hashTableSize).fill(null).map(() => new LinkList());
+    this.buckets = new Array(hashTableSize).fill(null).map(() => new Array());
     this.keys = {};
 }
 
@@ -14,11 +14,40 @@ HashTable.prototype = {
     set: function(key, value) {
         const keyHash = this.hash(key);
         this.keys[key] = keyHash;
-        const bucketLinkedList = this.buckets[keyHash];
-        
+        const bucketList = this.buckets[keyHash];
+        const node = bucketList.find(item => item.key === key);
+
+        if (!node) {
+            bucketList.push({key: key, value: value});
+        } else {
+            node.value = value;
+        }
+        return this;
     },
-    delete: function(key) {},
-    get: function(key) {},
-    has: function(key) {},
-    getKeys: function() {}
+    delete: function(key) {
+        const keyHash = this.hash(key);
+        delete this.keys[key];
+        const bucketList = this.buckets[keyHash];
+        const nodeIndex = bucketList.findIndex(item => item.key === key);
+
+        if (~nodeIndex) {
+            bucketList.splice(nodeIndex, 1);
+        }
+        return this;
+    },
+    get: function(key) {
+        const keyHash = this.hash(key);
+        const bucketList = this.buckets[keyHash];
+        const node = bucketList && bucketList.find(item => item.key === key);
+        if (node) return node.value;
+    },
+    has: function(key) {
+        return Object.prototype.hasOwnProperty.call(this.keys, key);
+    },
+    getKeys: function() {
+        return Object.keys(this.keys);
+    },
+    print: function() {
+        return JSON.stringify(this.buckets);
+    }
 }
