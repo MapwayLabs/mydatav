be5b1084c0d28f9bbe932da872afc870acca0c81
linghuam <1551827898@qq.com>
2019-05-28 19:22:28 +0800

    m

---
+++ webgl/3d-force-graph/data/data3.js
@@ -0,0 +1,9 @@
+// var n = this.GraphSDK.graph.nodes.map(n => ({id: n.id}));
+// JSON.stringify(n);
+// var e = this.GraphSDK.graph.edges.map(e => ({source: e.sourceId, target: e.targetId}));
+// JSON.stringify(e);
+
+const GraphData = {
+	nodes: [{"id":"35"},{"id":"1532"}],
+	links: [{"source":"1532","target":"35"}, {"source":"35","target":"1532"}]
+};
