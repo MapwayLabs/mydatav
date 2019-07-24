7f14f974258facc9550e96404196a2986810836a
linghuam <1551827898@qq.com>
2019-07-24 14:25:49 +0800

    add test

---
+++ esri/ezMap/test.js
@@ -0,0 +1,23 @@
+var customTileLayer = L.TileLayer.extend({
+    getTileUrl: function (coords) {
+		var data = {
+			x: coords.x + 1706,
+			y: coords.y + 304,
+			z: this._getZoomForUrl()
+		};
+		if (this._map && !this._map.options.crs.infinite) {
+			var invertedY = this._globalTileRange.max.y - coords.y;
+			if (this.options.tms) {
+				data['y'] = invertedY;
+			}
+			data['-y'] = invertedY;
+		}
+		return L.Util.template(this._url, data);
+    }
+});
+
+new customTileLayer('', {
+    zoomOffset: 4,
+    tms: true,
+    zoomReverse: false, // default
+}).addTo(map);
